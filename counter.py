04a6458d69b77ec2053c77241f74e711c446ec79
Rory Baxter <rorybaxter99@btinternet.com>
2021-04-12 10:36:48 +0100

    Added last week argument
    Added ability to get details from just the previous week, i.e., the week that has just finished

--- counter.py
+++ counter.py
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--culumative", action="store_true", help="Switch to displaying culumative effort instead of weekly effort")
 parser.add_argument("-e", "--exclude", action="store_true", help="Excludes the current week's activities from the total")
 parser.add_argument("-i", "--individual", action="store_true", help="In addition to the total time, also prints out the individual breakdown")
+parser.add_argument("-l", "--last", action="store_true", help="Gets the details for last week's activities")
 args = parser.parse_args()
 
 start_time = 1615032000
@@ -85,7 +86,6 @@ r = requests.get(request_url, params=params, headers=headers)
 current_activites = json.loads(r.text)
 activites_data += current_activites
 
-
 ''' This is my attempt to get multiple pages of information, but it seems to always return the same page
 while len(current_activites) == per_page:
     page += 1
@@ -129,6 +129,35 @@ if args.exclude:
         times[athelte] = times2[athelte] - times[athelte]
 
     string = "excluding this week"
+
+elif args.last:
+    activity_start_time = daylight_time_adjust(get_week_start_epoch()-time_in_a_week)
+    activity_end_time = activity_start_time + time_in_a_week
+
+    request_url = "https://www.strava.com/api/v3/clubs/" + str(club_id) + "/activities"
+    params = {"after": activity_start_time, "per_page": per_page, "page": page} 
+    headers = {"Authorization": "Bearer " + access}
+
+    times2 = {}
+
+    activites_data2 = []
+    r2 = requests.get(request_url, params=params, headers=headers)
+    current_activites2 = json.loads(r2.text)
+    activites_data2 += current_activites2
+
+    for activitiy in activites_data2:
+        name = activitiy["athlete"]["firstname"] + activitiy["athlete"]["lastname"]
+        times2[name] = times2.get(name, 0) + activitiy["moving_time"]
+
+    total_time2 = sum(times2.values())
+
+    total_time = total_time2-total_time
+
+    for athelte in times2:
+        times[athelte] = times2[athelte] - times.get(athelte, 0)
+
+    string = "last week"
+
 
 times = {name[0]: times[name[0]] for name in sorted(times.items(), key=lambda x: x[1], reverse=True)}
 readable_times = {name: str(datetime.timedelta(seconds=times[name])) for name in times.keys()}
